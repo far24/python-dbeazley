997b8f32616dd7c179a93fb4448f63ee835cc4e5
far24 <fahim.radi@gmail.com>
2020-07-10 21:42:45 -0400

    worked on bounce problem

--- practical-python/Work/bounce.py
+++ practical-python/Work/bounce.py
@@ -1,3 +1,13 @@
 # bounce.py
-#
+
+# initialization
+bounce_height = 100
+bounce_num = 0
+
+while bounce_num < 10:
+    bounce_height = bounce_height * (3/5)
+    bounce_num += 1
+    # print like table
+    print( bounce_num, round(bounce_height, 2))
+
 # Exercise 1.5
